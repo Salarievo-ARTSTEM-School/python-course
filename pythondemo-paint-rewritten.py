b702c646be35e939a3b37ba4527ee56998c5a776
Mikhail Veretennikov <36555298+mavergit@users.noreply.github.com>
2022-05-20 10:30:35 +0300

    Update pythondemo-paint-rewritten.py

--- pythondemo-paint-rewritten.py
+++ pythondemo-paint-rewritten.py
@@ -1,68 +1,46 @@
-#!/usr/bin/env python3
-"""       turtle-example-suite:
-
-            tdemo_paint.py
-
-A simple  event-driven paint program
-
-- left mouse button moves turtle
-- middle mouse button changes color
-- right mouse button toggles between pen up
-(no line drawn when the turtle moves) and
-pen down (line is drawn). If pen up follows
-at least two pen-down moves, the polygon that
-includes the starting point is filled.
- -------------------------------------------
- Play around by clicking into the canvas
- using all three mouse buttons.
- -------------------------------------------
-          To exit press STOP button
- -------------------------------------------
-"""
-from turtle import *
-
-screen=Screen()
-
-def switchupdown(x=0, y=0):
-    if pen()["pendown"]:
-        end_fill()
-        up()
-    else:
-        down()
-        begin_fill()
+import turtle
+t1=turtle.Turtle()
+s1=turtle.Screen()
 
 def changecolor(x=0, y=0):
     global colors
-    colors = colors[-1:]+colors[:-1]
-    color(colors[0])
+    colors = colors[1:]+colors[:1]
+    t1.color(colors[0])
     #print(colors)
 
-def reset():
-    screen.reset()
-    main()
+def switchupdown(x=0, y=0):
+    if t1.pen()["pendown"]:
+        t1.end_fill()
+        t1.up()
+    else:
+        t1.down()
+        t1.begin_fill()
 
-#def setpoint(x=0,y=0):
-#    circle(100)
-    
+
+def reset():
+    t1.clear()
+
 
 def main():
+    turtle.listen()
     global colors
-    shape("circle")
-    resizemode("user")
-    shapesize(.5)
-    width(3)
+    t1.shape("circle")
+    t1.resizemode("user")
+    t1.shapesize(.5)
+    t1.width(3)
     colors=["red", "green", "blue", "yellow"]
-    color(colors[0])
+    t1.color(colors[0])
     switchupdown()
-    onscreenclick(goto,1)
-    screen.onkey(changecolor,"space")
-    onscreenclick(switchupdown,2)
-    screen.onkey(reset,"c")
-#    ondrag(setpoint,1)
+    turtle.onscreenclick(t1.goto,1)
+    turtle.onkey(changecolor,"space")
+    s1.onclick(switchupdown,2)
+    turtle.onkey(reset,"c")
     return "EVENTLOOP"
+    
 
-if __name__ == "__main__":
-    msg = main()
-    listen()
-    print(msg)
-    mainloop()
+main()
+
+turtle.mainloop()
+
+
+
