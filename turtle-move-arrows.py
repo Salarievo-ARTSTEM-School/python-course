94b00c96ec4c92c301a975e8a15d3ed585ae5d9a
Mikhail Veretennikov <36555298+mavergit@users.noreply.github.com>
2022-05-20 10:24:10 +0300

    Update turtle-move-arrows.py

--- turtle-move-arrows.py
+++ turtle-move-arrows.py
@@ -2,19 +2,23 @@ import turtle
 
 def up():
     t.setheading(90)
-    t.forward(100)
+    t.forward(10)
+    #t.update()
 
 def down():
     t.setheading(270)
-    t.forward(100)
+    t.forward(10)
+    #t.update()
 
 def left():
     t.setheading(180)
-    t.forward(100)
+    t.forward(10)
+    #t.update()
 
 def right():
     t.setheading(0)
-    t.forward(100)
+    t.forward(10)
+    #t.update()
 
 turtle.onkey(up, "Up")  # This will call the up function if the "Left" arrow key is pressed
 turtle.onkey(down, "Down")
@@ -22,8 +26,8 @@ turtle.onkey(left, "Left")
 turtle.onkey(right, "Right")
 
 t=turtle.Turtle()
-t.fd(100)
+#t.fd(100)
 
 turtle.listen()
-
+turtle.update()
 turtle.mainloop() 
